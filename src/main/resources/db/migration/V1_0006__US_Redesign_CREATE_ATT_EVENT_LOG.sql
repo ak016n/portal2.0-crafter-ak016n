2cc2cd05dfc571fce000368bcec43604972fdc0f
so1234 <so1234@att.com>
2014-07-08 11:26:58 -0700

    event log table creation script.

---
+++ src/main/resources/db/migration/V1_0006__US_Redesign_CREATE_ATT_EVENT_LOG.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE IF NOT EXISTS dev_core.event_log (
+	id 					VARCHAR(40) NOT NULL PRIMARY KEY,
+	actor_id 			VARCHAR(250) NOT NULL,
+	impacted_user_id	VARCHAR(40),
+	org_id	 			VARCHAR(40),
+	event_id			INT NOT NULL,	
+	info				VARCHAR(4000),
+	actor_type			INT NOT NULL,
+	transaction_id      VARCHAR(40),
+	created_on		 	TIMESTAMP NOT NULL
+);
