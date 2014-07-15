ebab5ba61911ee175b091d3a5993d9cef4422c96
so1234 <so1234@att.com>
2014-07-15 16:44:35 -0700

    Date to Instant object.

--- src/integration/java/com/att/developer/dao/impl/JpaApiBundleDAOImplTest.java
+++ src/integration/java/com/att/developer/dao/impl/JpaApiBundleDAOImplTest.java
@@ -1,13 +1,20 @@
 package com.att.developer.dao.impl;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+import org.hamcrest.MatcherAssert;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
 import javax.annotation.Resource;
 import javax.transaction.Transactional;
 
 import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -29,28 +36,38 @@ public class JpaApiBundleDAOImplTest {
     private ApiBundleDAO apiBundleDAO;
 	
 	@Test
-	public void testCRUD() {
+	public void testCRUD() throws Exception{
 		// create
 		String userId = UUID.randomUUID().toString();
 		ApiBundle apiBundle = new ApiBundle(userId);
 		apiBundle.setName("b " + userId);
 		apiBundle.setComments("something nice");
+		apiBundle.setStartDate(Instant.now());
 		apiBundleDAO.create(apiBundle);
 		
 		// read
 		ApiBundle afterCreateApiBundle = new ApiBundle(userId);
 		afterCreateApiBundle = apiBundleDAO.load(afterCreateApiBundle);
-		assertThat(afterCreateApiBundle.getCreatedOn(), CoreMatchers.equalTo(apiBundle.getCreatedOn()));
-		assertThat(afterCreateApiBundle.getName(), CoreMatchers.equalTo(apiBundle.getName()));
+		MatcherAssert.assertThat(afterCreateApiBundle.getCreatedOn(), CoreMatchers.equalTo(apiBundle.getCreatedOn()));
+		MatcherAssert.assertThat(afterCreateApiBundle.getStartDate(), CoreMatchers.equalTo(apiBundle.getStartDate()));
+		MatcherAssert.assertThat(afterCreateApiBundle.getName(), CoreMatchers.equalTo(apiBundle.getName()));
 		
 		// update
 		afterCreateApiBundle.setName("xb " + userId);
+		LocalDateTime nowLocal = LocalDateTime.now().plus(1, ChronoUnit.YEARS);
+//		afterCreateApiBundle.setStartDate(Instant.now().plus(365*24*60*60*10000, ChronoUnit.MILLIS));
+		afterCreateApiBundle.setStartDate(nowLocal.toInstant(ZoneOffset.ofHours(-8)));
 		ApiBundle afterUpdate = apiBundleDAO.update(afterCreateApiBundle);
-		assertThat(afterUpdate.getName(), CoreMatchers.equalTo(afterCreateApiBundle.getName()));
+		MatcherAssert.assertThat(afterUpdate.getName(), CoreMatchers.equalTo(afterCreateApiBundle.getName()));
+		MatcherAssert.assertThat(afterUpdate.getStartDate(), CoreMatchers.equalTo(afterCreateApiBundle.getStartDate()));
+		
+		LocalDateTime localTimeAfterUpdate = LocalDateTime.ofInstant(afterUpdate.getStartDate(), ZoneId.of("America/Los_Angeles"));
+		
+		Assert.assertEquals(2015, localTimeAfterUpdate.get(ChronoField.YEAR));
 		
 		// delete
 		apiBundleDAO.delete(afterUpdate);
-		assertThat(apiBundleDAO.load(afterCreateApiBundle), CoreMatchers.nullValue());
+		MatcherAssert.assertThat(apiBundleDAO.load(afterCreateApiBundle), CoreMatchers.nullValue());
 	}
 	
 }
