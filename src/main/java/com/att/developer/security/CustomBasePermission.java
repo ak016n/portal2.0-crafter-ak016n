f74486b9ecbbff7e850de719ef95b9931f57cc4c
so1234 <so1234@att.com>
2014-07-16 11:21:58 -0700

    check for duplicate instances.

--- src/main/java/com/att/developer/security/CustomBasePermission.java
+++ src/main/java/com/att/developer/security/CustomBasePermission.java
@@ -3,11 +3,20 @@ package com.att.developer.security;
 import org.springframework.security.acls.domain.BasePermission;
 import org.springframework.security.acls.model.Permission;
 
+
+/**
+ * Extension of the BasePermission class to allow for combination of permissions. 
+ * 
+ * You can either use the READ_WRITE combo or pass in a list of Permission objects to the instanceOf 
+ * 
+ * @author so1234
+ *
+ */
 public class CustomBasePermission extends BasePermission {
 
 	private static final long serialVersionUID = 5162279896437833577L;
 	
-	public static final Permission READ_WRITE = new CustomBasePermission(3);
+	public static final CustomBasePermission READ_WRITE = new CustomBasePermission(3);
 
 	protected CustomBasePermission(int mask) {
 		super(mask);
@@ -21,6 +30,11 @@ public class CustomBasePermission extends BasePermission {
 		int mask = 0;
 		for(Permission permission : permissions){
 			mask = mask | permission.getMask();
+		}
+
+		//make sure we are not returning existing READ_WRITE. 
+		if(mask == READ_WRITE.getMask()){
+			return READ_WRITE;
 		}
 		
 		return new CustomBasePermission(mask);
