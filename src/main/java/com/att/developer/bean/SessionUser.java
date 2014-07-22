356c9aa93ad8021c6e63f733f0b992c7dc5f27d2
so1234 <so1234@att.com>
2014-07-22 11:39:35 -0700

    javadoc around username change.

--- src/main/java/com/att/developer/bean/SessionUser.java
+++ src/main/java/com/att/developer/bean/SessionUser.java
@@ -4,19 +4,26 @@ import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
 
+/**
+ * We are using userId which is a generated UUID for the 'username' that Spring
+ * requires. This guarantees that if we ever want to change the username, it
+ * will not break the underlying Spring security ACL mappings as those mappings
+ * will be using
+ * 
+ * @author som
+ *
+ */
 public class SessionUser extends org.springframework.security.core.userdetails.User {
 
 	private static final long serialVersionUID = -992872897986116196L;
-	
+
 	private User user;
-	
-	public SessionUser(String username, String password, boolean enabled,
-			boolean accountNonExpired, boolean credentialsNonExpired,
-			boolean accountNonLocked,
+
+	public SessionUser(String username, String password, boolean enabled, boolean accountNonExpired,
+			boolean credentialsNonExpired, boolean accountNonLocked,
 			Collection<? extends GrantedAuthority> authorities, User user) {
-		super(username, password, enabled, accountNonExpired, credentialsNonExpired,
-				accountNonLocked, authorities);
-		this.user = user; 
+		super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+		this.user = user;
 	}
 
 	public User getUser() {
