f961e7bf775e31371047c62625a0d50dfebb81a7
so1234 <so1234@att.com>
2014-07-23 15:45:34 -0700

    add generic writeEvent.

--- src/main/java/com/att/developer/service/EventTrackingService.java
+++ src/main/java/com/att/developer/service/EventTrackingService.java
@@ -5,4 +5,6 @@ import com.att.developer.bean.EventLog;
 public interface EventTrackingService {
 
 	public void globalPropertiesChangeEvent(EventLog eventLog);
+
+	public void writeEvent(EventLog eventLog);
 }
