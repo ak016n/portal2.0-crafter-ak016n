a1bceed19aefd68327f3396914d2dc2b3ec8a144
so1234 <so1234@att.com>
2014-07-23 18:10:26 -0700

    equals, hashCode better.

--- src/main/java/com/att/developer/bean/ApiBundle.java
+++ src/main/java/com/att/developer/bean/ApiBundle.java
@@ -2,6 +2,7 @@ package com.att.developer.bean;
 
 import java.time.Instant;
 import java.util.Date;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -142,10 +143,7 @@ public class ApiBundle {
 	
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
+		return Objects.hashCode(this.id);
 	}
 
 	
@@ -161,12 +159,7 @@ public class ApiBundle {
 			return false;
 		}
 		ApiBundle other = (ApiBundle) obj;
-		if(this.getId().equals(other.getId())){
-			return true;
-		}
-		else{
-			return false;
-		}
+		return Objects.equals(this.id, other.id);
 	}
 
 	
