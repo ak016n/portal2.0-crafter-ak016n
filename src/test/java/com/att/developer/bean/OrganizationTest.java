d0e926095bb1722cd426ad6020354d924166de2d
so1234 <so1234@att.com>
2014-08-06 18:07:21 -0700

    format to stnadard.

--- src/test/java/com/att/developer/bean/OrganizationTest.java
+++ src/test/java/com/att/developer/bean/OrganizationTest.java
@@ -1,6 +1,5 @@
 package com.att.developer.bean;
 
-
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,22 +10,21 @@ import com.att.developer.bean.builder.OrganizationBuilder;
 import com.att.developer.bean.builder.RoleBuilder;
 import com.att.developer.bean.builder.UserBuilder;
 
-
 public class OrganizationTest {
 
-	@Test
-	public void testGetOrganizationAdmin() {
-		Role opaRole = new RoleBuilder().withName(Role.ROLE_NAME_ORG_ADMIN).build();
-					
-		User userPenny = new UserBuilder().withLogin("penny_test").withRole(opaRole).build();
-		User vanillaUser = new UserBuilder().withVanillaUser().build();
-		Set<User> users = new HashSet<>();
-		users.add(userPenny);
-		users.add(vanillaUser);
-		
-		Organization org = new OrganizationBuilder().withUsers(users).build();
-		User adminUser = org.getOrganizationAdmin();
-		
-		Assert.assertEquals("penny is NOT the admin", userPenny,  adminUser);
-	}
+    @Test
+    public void testGetOrganizationAdmin() {
+        Role opaRole = new RoleBuilder().withName(Role.ROLE_NAME_ORG_ADMIN).build();
+
+        User userPenny = new UserBuilder().withLogin("penny_test").withRole(opaRole).build();
+        User vanillaUser = new UserBuilder().withVanillaUser().build();
+        Set<User> users = new HashSet<>();
+        users.add(userPenny);
+        users.add(vanillaUser);
+
+        Organization org = new OrganizationBuilder().withUsers(users).build();
+        User adminUser = org.getOrganizationAdmin();
+
+        Assert.assertEquals("penny is NOT the admin", userPenny, adminUser);
+    }
 }
