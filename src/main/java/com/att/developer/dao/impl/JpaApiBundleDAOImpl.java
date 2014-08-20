5494791e096ee690aa351b4a9c188d7587d04c3b
so1234 <so1234@att.com>
2014-08-20 09:09:17 -0700

    format to standard.

--- src/main/java/com/att/developer/dao/impl/JpaApiBundleDAOImpl.java
+++ src/main/java/com/att/developer/dao/impl/JpaApiBundleDAOImpl.java
@@ -15,29 +15,27 @@ import com.att.developer.dao.ApiBundleDAO;
 @Component
 public class JpaApiBundleDAOImpl extends JpaDAO<ApiBundle> implements ApiBundleDAO {
 
-	private static final String QUERY_ALL_BUNDLES = "from ApiBundle as a";
-	
-	private final Logger logger = LogManager.getLogger();
-	
-	 
-	public JpaApiBundleDAOImpl() {
-		super(ApiBundle.class);
-	}
+    private static final String QUERY_ALL_BUNDLES = "from ApiBundle as a";
 
-	
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<ApiBundle> getAll() {
-		Query query = entityManager.createQuery(QUERY_ALL_BUNDLES);
+    private final Logger logger = LogManager.getLogger();
+
+    public JpaApiBundleDAOImpl() {
+        super(ApiBundle.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<ApiBundle> getAll() {
+        Query query = entityManager.createQuery(QUERY_ALL_BUNDLES);
         List<ApiBundle> allBundles = null;
         try {
-        	allBundles = query.getResultList();
+            allBundles = query.getResultList();
         } catch (NoResultException e) {
             // ok to swallow
-        	logger.debug("found no ApiBundles at ALL!!");
+            logger.debug("found no ApiBundles at ALL!!");
         } catch (Exception e) {
-        	logger.error(e);
+            logger.error(e);
         }
         return allBundles;
-	}
+    }
 }
