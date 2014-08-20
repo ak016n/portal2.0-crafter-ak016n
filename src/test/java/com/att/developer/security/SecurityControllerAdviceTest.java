9ab215b583f3f792981a1843f82249c6a2b2c319
so1234 <so1234@att.com>
2014-08-20 09:57:25 -0700

    more tests

--- src/test/java/com/att/developer/security/SecurityControllerAdviceTest.java
+++ src/test/java/com/att/developer/security/SecurityControllerAdviceTest.java
@@ -1,19 +1,63 @@
 package com.att.developer.security;
 
-import org.junit.Assert;
+import java.util.Collections;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import com.att.developer.bean.Role;
 import com.att.developer.bean.SessionUser;
+import com.att.developer.bean.User;
+import com.att.developer.bean.builder.RoleBuilder;
+import com.att.developer.bean.builder.UserBuilder;
 
 public class SecurityControllerAdviceTest {
 
+    private User actor = null;
+
+    private static final String USER_NOT_PRIVILEGED_ID = "unprivilegedId";
+
+    @Before
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+
+        Role unprivilegedRole = new RoleBuilder().withName("notPrilegedRole").build();
+        actor = new UserBuilder().withRole(unprivilegedRole).withId(USER_NOT_PRIVILEGED_ID).build();
+    }
+
     @Test
-    public void testCurrentUser() {
+    public void testCurrentUser_nullAuthentication() {
         SecurityControllerAdvice advice = new SecurityControllerAdvice();
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        SessionUser sessionUser = advice.currentUser(authentication);
+        Assert.assertNull(sessionUser);
+    }
+
+    @Test
+    public void testCurrentUser_sessionUserAuthentication() {
+        SecurityControllerAdvice advice = new SecurityControllerAdvice();
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(new SessionUser(actor), actor.getPassword(), AuthorityUtils.createAuthorityList("BogusRole"));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        SessionUser sessionUser = advice.currentUser(authentication);
+        Assert.assertNotNull(sessionUser);
+    }
+
+    
+    @Test
+    public void testCurrentUser_prncipalStringAuthentication() {
+        SecurityControllerAdvice advice = new SecurityControllerAdvice();
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken("someStringPrincipal", "password", AuthorityUtils.createAuthorityList("BogusRole"));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
         SessionUser sessionUser = advice.currentUser(authentication);
         Assert.assertNull(sessionUser);
     }
