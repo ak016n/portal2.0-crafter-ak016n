f3a7aaecd1629e0c8d68206e1e501111854f1250
som <ss380m@att.com>
2014-08-22 15:07:11 -0700

    removing @Component

--- src/main/java/com/att/developer/jms/consumer/EventLogConsumer.java
+++ src/main/java/com/att/developer/jms/consumer/EventLogConsumer.java
@@ -2,12 +2,9 @@ package com.att.developer.jms.consumer;
 
 import javax.inject.Inject;
 
-import org.springframework.stereotype.Component;
-
 import com.att.developer.bean.EventLog;
 import com.att.developer.service.EventLogService;
 
-@Component
 public class EventLogConsumer {
 
 	@Inject
