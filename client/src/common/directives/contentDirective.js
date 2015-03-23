44ea35981c84b980386c738dfbc39dc57c185088
som sivaraj <ss380m@att.com>
2015-03-23 10:50:35 -0700

    Code clean up: Content Directives

--- client/src/common/directives/contentDirective.js
+++ client/src/common/directives/contentDirective.js
@@ -1,14 +1,17 @@
-angular.module('portalApp').directive('contentItem', ['$http', '$compile', '$templateCache', function ($http, $compile, $templateCache) {
-    return {
-        restrict: "E",
-        replace: true,
-        template: '<div class="controls"></div>',
-        link: function(scope, element, attrs) {
-        	var template = $compile($templateCache.get('app/content/' + scope.content.templateUrl))(scope);
-            element.append(template);
-        },
-        scope: {
-            content:'='
-        }
-    };
-}]);
+(function() {
+    'use strict';
+	angular.module('portalApp').directive('contentItem', ['$http', '$compile', '$templateCache', function ($http, $compile, $templateCache) {
+	    return {
+	        restrict: "E",
+	        replace: true,
+	        template: '<div class="controls"></div>',
+	        link: function(scope, element, attrs) {
+	        	var template = $compile($templateCache.get('app/content/' + scope.content.templateUrl))(scope);
+	            element.append(template);
+	        },
+	        scope: {
+	            content:'='
+	        }
+	    };
+	}]);
+})();
