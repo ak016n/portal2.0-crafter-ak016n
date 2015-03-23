bd9c0435f200a9ea97d0961f9472628e0a2ebc10
som sivaraj <ss380m@att.com>
2015-03-23 10:51:20 -0700

    Code clean up: Common security logic

--- client/src/common/security/securityInterceptor.js
+++ client/src/common/security/securityInterceptor.js
@@ -1,30 +1,35 @@
-/**
- * Http Interceptor for authentication request - 401
- */
-angular.module('security').config(['$httpProvider', function($httpProvider, $stateProvider) {
-	$httpProvider.interceptors.push(['$q', '$rootScope', '$sessionStorage', '$location', function($q, $rootScope, $sessionStorage, $location) {
-		return {
-			'responseError' : function(rejection) {
-				var status = rejection.status;
-				var config = rejection.config;
-				var method = config.method;
-				var url = config.url;
+(function() {
+    'use strict';
 
-				if (angular.isUndefined($sessionStorage.accessToken) && status === 401) {
-					$sessionStorage.destUrl = $location.path();
-					$location.path("/login");
-				} else {
-					$rootScope.error = method + " on " + url + " failed with status " + status;
+	/**
+	 * Http Interceptor for authentication request - 401
+	 */
+	angular.module('security').config(['$httpProvider', function($httpProvider, $stateProvider) {
+		$httpProvider.interceptors.push(['$q', '$rootScope', '$sessionStorage', '$location', function($q, $rootScope, $sessionStorage, $location) {
+			return {
+				'responseError' : function(rejection) {
+					var status = rejection.status;
+					var config = rejection.config;
+					var method = config.method;
+					var url = config.url;
+	
+					if (angular.isUndefined($sessionStorage.accessToken) && status === 401) {
+						$sessionStorage.destUrl = $location.path();
+						$location.path("/login");
+					} else {
+						$rootScope.error = method + " on " + url + " failed with status " + status;
+					}
+	
+					return $q.reject(rejection);
+				},
+				request : function(config) {
+					if(angular.isDefined($sessionStorage.accessToken)) {
+						config.headers['Authorization'] = 'Bearer ' + $sessionStorage.accessToken;
+					}
+					return config;
 				}
-
-				return $q.reject(rejection);
-			},
-			request : function(config) {
-				if(angular.isDefined($sessionStorage.accessToken)) {
-					config.headers['Authorization'] = 'Bearer ' + $sessionStorage.accessToken;
-				}
-				return config;
-			}
-		};
+			};
+		}]);
 	}]);
-}]);
+
+})();
