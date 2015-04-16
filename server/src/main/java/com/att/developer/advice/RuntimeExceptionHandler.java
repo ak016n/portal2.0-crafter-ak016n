008c6c110396751d76d3849f3c9838d403a1972a
som sivaraj <ss380m@att.com>
2015-04-16 10:21:23 -0700

    Fix for double encoding

--- server/src/main/java/com/att/developer/advice/RuntimeExceptionHandler.java
+++ server/src/main/java/com/att/developer/advice/RuntimeExceptionHandler.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.att.developer.bean.ServerSideError;
 import com.att.developer.bean.ServerSideErrors;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ControllerAdvice
 @Order(Ordered.LOWEST_PRECEDENCE)
@@ -19,15 +18,13 @@ public class RuntimeExceptionHandler {
 
 	@ExceptionHandler(RuntimeException.class)
 	@ResponseBody
-	public ResponseEntity<String> handleRuntimeException(RuntimeException e) throws JsonProcessingException {
+	public ResponseEntity<ServerSideErrors> handleRuntimeException(RuntimeException e) throws JsonProcessingException {
 		ServerSideErrors errors = new ServerSideErrors();
 		String errorMessage = (e.getCause() == null) ? e.getMessage() : e.getCause().getMessage();
 		ServerSideError error = new ServerSideError.Builder().id("Unexpected").message(errorMessage).build();
 		errors.add(error);
 		
-		ObjectMapper jsonMapper = new ObjectMapper();
-		
-		return new ResponseEntity<String>(jsonMapper.writeValueAsString(errors), HttpStatus.INTERNAL_SERVER_ERROR);
+		return new ResponseEntity<ServerSideErrors>(errors, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 }
