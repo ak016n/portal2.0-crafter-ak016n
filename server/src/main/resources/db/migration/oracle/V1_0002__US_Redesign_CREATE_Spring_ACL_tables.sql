fdd82557ce25698caf53533e9e39b67e4120f843
som sivaraj <ss380m@att.com>
2015-04-21 16:25:54 -0700

    upgraded spring acl table

--- server/src/main/resources/db/migration/oracle/V1_0002__US_Redesign_CREATE_Spring_ACL_tables.sql
+++ server/src/main/resources/db/migration/oracle/V1_0002__US_Redesign_CREATE_Spring_ACL_tables.sql
@@ -1,67 +1,88 @@
+-- ACL Schema SQL for Oracle Database 10g+
+-- drop trigger acl_sid_id_trigger;
+-- drop trigger acl_class_id_trigger;
+-- drop trigger acl_object_identity_id_trigger;
+-- drop trigger acl_entry_id_trigger;
+-- drop sequence acl_sid_sequence;
+-- drop sequence acl_class_sequence;
+-- drop sequence acl_object_identity_sequence;
+-- drop sequence acl_entry_sequence;
+-- drop table acl_entry;
+-- drop table acl_object_identity;
+-- drop table acl_class;
+-- drop table acl_sid;
+CREATE TABLE acl_sid (
+id NUMBER(38) NOT NULL PRIMARY KEY,
+principal NUMBER(1) NOT NULL CHECK (principal in (0, 1)),
+sid NVARCHAR2(100) NOT NULL,
+CONSTRAINT unique_acl_sid UNIQUE (sid, principal)
+);
+
+CREATE SEQUENCE acl_sid_sequence START WITH 1 INCREMENT BY 1 NOMAXVALUE;
+
+CREATE OR REPLACE TRIGGER acl_sid_id_trigger
+BEFORE INSERT ON acl_sid
+FOR EACH ROW
 BEGIN
-	execute immediate 'DROP TABLE acl_entry';
-	execute immediate 'DROP TABLE acl_object_identity';
-	execute immediate 'DROP TABLE acl_sid';
-	execute immediate 'DROP TABLE acl_class';
-	execute immediate 'DROP SEQUENCE acl_sid_sequence';
-	execute immediate 'DROP SEQUENCE acl_sid_sequence';
-	execute immediate 'DROP SEQUENCE acl_class_sequence';
-EXCEPTION
-  WHEN OTHERS THEN
-	NULL;
-  END;
-/
+SELECT acl_sid_sequence.nextval INTO :new.id FROM dual;
+END;
 
-CREATE TABLE acl_sid( id INT NOT NULL,
-principal NUMBER(1) NOT NULL,
-sid VARCHAR(100) NOT NULL,
-CONSTRAINT acl_sid_pk PRIMARY KEY (id));
+CREATE TABLE acl_class (
+id NUMBER(38) NOT NULL PRIMARY KEY,
+class NVARCHAR2(100) NOT NULL,
+CONSTRAINT uk_acl_class UNIQUE (class)
+);
 
-CREATE TABLE acl_class( id INT NOT NULL,
-class VARCHAR(100) NOT NULL,
-CONSTRAINT acl_class_pk PRIMARY KEY (id),
-CONSTRAINT acl_class_unique_class UNIQUE(class));
- 
-CREATE TABLE acl_object_identity( id int NOT NULL,
-object_id_class INT NOT NULL,
+CREATE SEQUENCE acl_class_sequence START WITH 1 INCREMENT BY 1 NOMAXVALUE;
+
+CREATE OR REPLACE TRIGGER acl_class_id_trigger
+BEFORE INSERT ON acl_class
+FOR EACH ROW
+BEGIN
+SELECT acl_class_sequence.nextval INTO :new.id FROM dual;
+END;
+
+CREATE TABLE acl_object_identity (
+id NUMBER(38) NOT NULL PRIMARY KEY,
+object_id_class NUMBER(38) NOT NULL,
 object_id_identity VARCHAR(80) NOT NULL,
-parent_object INT,
-owner_sid INT,
-entries_inheriting number(1) NOT NULL,
-CONSTRAINT acl_o_i_pk PRIMARY KEY (id),
-CONSTRAINT acl_o_i_unique_class_oid UNIQUE(object_id_class,object_id_identity),
-CONSTRAINT fk_aoi_parent_object FOREIGN KEY(PARENT_OBJECT) REFERENCES acl_object_identity(id),
-CONSTRAINT fk_aoi_acl_class FOREIGN KEY(object_id_class) REFERENCES acl_class(id),
-CONSTRAINT fk_aoi_acl_sid FOREIGN KEY(OWNER_SID) REFERENCES acl_sid(id));
+parent_object NUMBER(38),
+owner_sid NUMBER(38),
+entries_inheriting NUMBER(1) NOT NULL CHECK (entries_inheriting in (0, 1)),
+CONSTRAINT uk_acl_object_identity UNIQUE (object_id_class, object_id_identity),
+CONSTRAINT fk_acl_object_identity_parent FOREIGN KEY (parent_object) REFERENCES acl_object_identity (id),
+CONSTRAINT fk_acl_object_identity_class FOREIGN KEY (object_id_class) REFERENCES acl_class (id),
+CONSTRAINT fk_acl_object_identity_owner FOREIGN KEY (owner_sid) REFERENCES acl_sid (id)
+);
 
-CREATE TABLE acl_entry( id INT NOT NULL PRIMARY KEY,
-acl_object_identity INT NOT NULL,
-ace_order INT NOT NULL,
-sid INT NOT NULL,
+CREATE SEQUENCE acl_object_identity_sequence START WITH 1 INCREMENT BY 1 NOMAXVALUE;
+
+CREATE OR REPLACE TRIGGER acl_object_identity_id_trigger
+BEFORE INSERT ON acl_object_identity
+FOR EACH ROW
+BEGIN
+SELECT acl_object_identity_sequence.nextval INTO :new.id FROM dual;
+END;
+
+CREATE TABLE acl_entry (
+id NUMBER(38) NOT NULL PRIMARY KEY,
+acl_object_identity NUMBER(38) NOT NULL,
+ace_order INTEGER NOT NULL,
+sid NUMBER(38) NOT NULL,
 mask INTEGER NOT NULL,
-granting number(1) NOT NULL,
-audit_success number(1) NOT NULL,
-audit_failure number(1) NOT NULL,
-CONSTRAINT acl_entry_unique_oid_order UNIQUE(acl_object_identity,ace_order),
-CONSTRAINT fk_acl_entry_aoi FOREIGN KEY(acl_object_identity) REFERENCES acl_object_identity(id),
-CONSTRAINT fk_acl_entry_acl_sid FOREIGN KEY(SID) REFERENCES acl_sid(id));
+granting NUMBER(1) NOT NULL CHECK (granting in (0, 1)),
+audit_success NUMBER(1) NOT NULL CHECK (audit_success in (0, 1)),
+audit_failure NUMBER(1) NOT NULL CHECK (audit_failure in (0, 1)),
+CONSTRAINT unique_acl_entry UNIQUE (acl_object_identity, ace_order),
+CONSTRAINT fk_acl_entry_object FOREIGN KEY (acl_object_identity) REFERENCES acl_object_identity (id),
+CONSTRAINT fk_acl_entry_acl FOREIGN KEY (sid) REFERENCES acl_sid (id)
+);
 
-CREATE SEQUENCE acl_sid_sequence;
+CREATE SEQUENCE acl_entry_sequence START WITH 1 INCREMENT BY 1 NOMAXVALUE;
 
-CREATE OR REPLACE TRIGGER acl_sid_id
-BEFORE INSERT ON acl_sid
+CREATE OR REPLACE TRIGGER acl_entry_id_trigger
+BEFORE INSERT ON acl_entry
 FOR EACH ROW
 BEGIN
-SELECT acl_sid_sequence.NEXTVAL INTO :new.id FROM dual;
+SELECT acl_entry_sequence.nextval INTO :new.id FROM dual;
 END;
-/
-
-CREATE SEQUENCE acl_class_sequence;
-
-CREATE OR REPLACE TRIGGER acl_class_id
-BEFORE INSERT ON acl_sid
-FOR EACH ROW
-BEGIN
-SELECT acl_class_sequence.NEXTVAL INTO :new.id FROM dual;
-END;
-/
