488933fd4de4bee198b185a91efdf8d71c47a94c
som sivaraj <ss380m@att.com>
2015-04-21 16:07:23 -0700

    created synonyms

---
+++ server/src/main/resources/db/migration/oracle/V1_0003__US_Redesign_CREATE_INITIAL_SYNONYMS.sql
@@ -0,0 +1,22 @@
+
+CREATE OR REPLACE SYNONYM appdevcore.att_user FOR devcore.att_user;
+
+CREATE OR REPLACE SYNONYM appdevcore.att_properties FOR devcore.att_properties;
+
+CREATE OR REPLACE SYNONYM appdevcore.att_event_log FOR devcore.att_event_log;
+
+CREATE OR REPLACE SYNONYM appdevcore.att_organization FOR devcore.att_organization;
+
+CREATE OR REPLACE SYNONYM appdevcore.att_user_org_membership FOR devcore.att_user_org_membership;
+
+CREATE OR REPLACE SYNONYM appdevcore.att_state FOR devcore.att_state;
+
+CREATE OR REPLACE SYNONYM appdevcore.att_api_bundle FOR devcore.att_api_bundle;
+
+CREATE OR REPLACE SYNONYM appdevcore.att_role FOR devcore.att_role;
+
+CREATE OR REPLACE SYNONYM appdevcore.att_user_role_relationship FOR devcore.att_user_role_relationship;
+
+CREATE OR REPLACE SYNONYM appdevcore.att_org_role_relationship FOR devcore.att_org_role_relationship;
+
+COMMIT;
