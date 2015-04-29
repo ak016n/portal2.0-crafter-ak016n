fbb4b7a31e1b3ff6251877ece19313e9dabf23eb
som sivaraj <ss380m@att.com>
2015-04-29 15:29:13 -0700

    Enhance Blog Post to include Categories and Tags

---
+++ server/src/main/java/com/att/developer/bean/blog/BlogMetadata.java
@@ -0,0 +1,16 @@
+package com.att.developer.bean.blog;
+
+
+public class BlogMetadata {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
