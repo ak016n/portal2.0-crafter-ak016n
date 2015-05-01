84d2328c23e16532afcf4a0efaa4aa3b9a0c6928
som sivaraj <ss380m@att.com>
2015-05-01 12:48:05 -0700

    updates to sql

--- server/src/main/resources/db/migration/oracle/V1_0007__US_Redesign_ALTER_ORGANIZATION_TABLES.sql
+++ server/src/main/resources/db/migration/oracle/V1_0007__US_Redesign_ALTER_ORGANIZATION_TABLES.sql
@@ -1,4 +1,4 @@
-ALTER TABLE dev_core.organization ADD (organization_type VARCHAR2(2) not null default '0');
+ALTER TABLE dev_core.organization ADD (organization_type VARCHAR2(2) default '0' not null);
 
 UPDATE dev_core.organization set organization_type = '3';
 
